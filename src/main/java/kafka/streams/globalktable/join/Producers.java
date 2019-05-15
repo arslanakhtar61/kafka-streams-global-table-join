48b9c4133e938c483d6ce18c6950e5de06bb3b50
Muhammad Arslan Akhtar <arslanakhtar61@gmail.com>
2019-05-15 10:39:22 +0800

    generate messages for topics from which stateStore will be materializedAs on app startup to avoid stream timing issues

--- src/main/java/kafka/streams/globalktable/join/Producers.java
+++ src/main/java/kafka/streams/globalktable/join/Producers.java
@@ -41,49 +41,9 @@ public class Producers {
 		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
 		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
 		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
 
-		List<KeyValue<String, Long>> userClicks = Arrays.asList(
-				new KeyValue<>("alice", 13L),
-				new KeyValue<>("bob", 4L),
-				new KeyValue<>("chao", 25L),
-				new KeyValue<>("bob", 19L),
-				new KeyValue<>("dave", 56L),
-				new KeyValue<>("eve", 78L),
-				new KeyValue<>("alice", 40L),
-				new KeyValue<>("fang", 99L)
-		);
-
-		DefaultKafkaProducerFactory<String, Long> pf = new DefaultKafkaProducerFactory<>(props);
-		KafkaTemplate<String, Long> template = new KafkaTemplate<>(pf, true);
-		template.setDefaultTopic("user-clicks3");
-
-		for (KeyValue<String,Long> keyValue : userClicks) {
-			template.sendDefault(keyValue.key, keyValue.value);
-		}
-
-		List<KeyValue<String, String>> userRegions = Arrays.asList(
-				new KeyValue<>("alice", "asia"),   /* Alice lived in Asia originally... */
-				new KeyValue<>("bob", "americas"),
-				new KeyValue<>("chao", "asia"),
-				new KeyValue<>("dave", "europe"),
-				new KeyValue<>("alice", "europe"), /* ...but moved to Europe some time later. */
-				new KeyValue<>("eve", "americas"),
-				new KeyValue<>("fang", "asia")
-		);
-
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-		DefaultKafkaProducerFactory<String, String> pf1 = new DefaultKafkaProducerFactory<>(props);
-		KafkaTemplate<String, String> template1 = new KafkaTemplate<>(pf1, true);
-		template1.setDefaultTopic("user-regions");
-
-		for (KeyValue<String,String> keyValue : userRegions) {
-			template1.sendDefault(keyValue.key, keyValue.value);
-		}
-
+		//https://github.com/spring-cloud/spring-cloud-stream-samples/issues/68#issuecomment-488294797
+		//Generate messages for topic region-top-exports, the topics from which stateStores will be materializedAS on app startup needs to be produced first to avoid stream timing issue.
 		List<KeyValue<String, String>> regionTopExport = Arrays.asList(
 				new KeyValue<>("asia", "rice"),
 				new KeyValue<>("americas", "beef"),
@@ -103,6 +63,51 @@ public class Producers {
 
 		for (KeyValue<String,String> keyValue : regionTopExport) {
 			template2.sendDefault(keyValue.key, keyValue.value);
+		}
+
+		//Generate messages for topic user-clicks3
+		List<KeyValue<String, Long>> userClicks = Arrays.asList(
+				new KeyValue<>("alice", 13L),
+				new KeyValue<>("bob", 4L),
+				new KeyValue<>("chao", 25L),
+				new KeyValue<>("bob", 19L),
+				new KeyValue<>("dave", 56L),
+				new KeyValue<>("eve", 78L),
+				new KeyValue<>("alice", 40L),
+				new KeyValue<>("fang", 99L)
+		);
+
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+
+		DefaultKafkaProducerFactory<String, Long> pf = new DefaultKafkaProducerFactory<>(props);
+		KafkaTemplate<String, Long> template = new KafkaTemplate<>(pf, true);
+		template.setDefaultTopic("user-clicks3");
+
+		for (KeyValue<String,Long> keyValue : userClicks) {
+			template.sendDefault(keyValue.key, keyValue.value);
+		}
+
+		//Generate messages for topic user-regions
+		List<KeyValue<String, String>> userRegions = Arrays.asList(
+				new KeyValue<>("alice", "asia"),   /* Alice lived in Asia originally... */
+				new KeyValue<>("bob", "americas"),
+				new KeyValue<>("chao", "asia"),
+				new KeyValue<>("dave", "europe"),
+				new KeyValue<>("alice", "europe"), /* ...but moved to Europe some time later. */
+				new KeyValue<>("eve", "americas"),
+				new KeyValue<>("fang", "asia")
+		);
+
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+		DefaultKafkaProducerFactory<String, String> pf1 = new DefaultKafkaProducerFactory<>(props);
+		KafkaTemplate<String, String> template1 = new KafkaTemplate<>(pf1, true);
+		template1.setDefaultTopic("user-regions");
+
+		for (KeyValue<String,String> keyValue : userRegions) {
+			template1.sendDefault(keyValue.key, keyValue.value);
 		}
 
 	}
